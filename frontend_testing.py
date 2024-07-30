7c3cfed67b1faf72e6670f27fb939a8cd95ccb73
danielS <daniel.schafman@gmail.com>
2024-07-30 22:07:21 +0300

    reuploading frontend_testing

--- frontend_testing.py
+++ frontend_testing.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from time import sleep
 
 options = Options()
 driver = webdriver.Chrome(options=options)
