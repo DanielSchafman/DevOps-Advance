c19933ea228d674cbc27914609745b4d5ed1cb41
danielS <daniel.schafman@gmail.com>
2024-07-30 18:40:18 +0300

    finished the frontend_testing file

--- frontend_testing.py
+++ frontend_testing.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
@@ -6,9 +7,10 @@ from time import sleep
 
 options = Options()
 driver = webdriver.Chrome(options=options)
-user_id = 2
+user_id = 1
 
 driver.get("http://127.0.0.1:5001/users/" + str(user_id))
 
-print(driver.title)
-sleep(5)
+user = driver.find_element(By.ID, "user").text
+print(user)
+driver.quit()
